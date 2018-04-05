8a4d970e2d3cc14d5d3b074e47768bb251215e8f
wenlaura <wenlaura@umich.edu>
2018-04-05 14:55:16 -0400

    Fixing python spacing

--- regression/ppmi_europarl.py
+++ regression/ppmi_europarl.py
@@ -14,40 +14,40 @@ EUROPARL_FILE = '/local/embedding_datasets/europarl/europarl_processed.txt' #All
 OUTPUT_FOLDER = '/local/embedding_datasets/europarl/metaClassifier/' #Where the embedding spaces should be saved
 
 def buildCooccurrenceMatrix(sentences,wordList,windowSize=5):
-	print('Building cooccurence matrix...')
-	cooccur = np.zeros((len(wordList)+1,len(wordList)+1)) #Last dimension is for unknown words
-	for sentence in sentences:
-		sentence = [wordList.index(i) if i in wordList else len(wordList) for i in sentence]
-		for i in range(len(sentence)-1):
-			for j in range(1,windowSize):
-				if i+j >= len(sentence):
-					break
-				cooccur[sentence[i]][sentence[i+j]] += 1
-				if sentence[i+j] != sentence[i]:
-					cooccur[sentence[i+j]][sentence[i]] += 1
-	
-	return cooccur
+   print('Building cooccurence matrix...')
+   cooccur = np.zeros((len(wordList)+1,len(wordList)+1)) #Last dimension is for unknown words
+   for sentence in sentences:
+      sentence = [wordList.index(i) if i in wordList else len(wordList) for i in sentence]
+      for i in range(len(sentence)-1):
+         for j in range(1,windowSize):
+            if i+j >= len(sentence):
+               break
+            cooccur[sentence[i]][sentence[i+j]] += 1
+            if sentence[i+j] != sentence[i]:
+               cooccur[sentence[i+j]][sentence[i]] += 1
+   
+   return cooccur
 
 def getPPMI(cooccur,wordList):
-	dMagnitude = 0
-	singleCounts = []
-	for i in range(len(cooccur)):
-		count = 0
-		for j in range(len(cooccur)):
-			count += cooccur[i][j]
-		singleCounts.append(count)
-		dMagnitude += count
-		
-	#Calculate ppmi for all entries in cooccur
-	ppmi = np.zeros((len(cooccur),len(cooccur)))
-	for i in range(len(cooccur)):
-		for j in range(len(cooccur)):
-			if i < j: #will be a symmetric matrix 
-				break
-			if cooccur[i][j] > 0:
-				pmi = math.log((cooccur[i][j]*dMagnitude)/(singleCounts[i]*singleCounts[j]),2)
-				if pmi > 0:
-					ppmi[i][j] = ppmi[j][i] = pmi
+   dMagnitude = 0
+   singleCounts = []
+   for i in range(len(cooccur)):
+      count = 0
+      for j in range(len(cooccur)):
+         count += cooccur[i][j]
+      singleCounts.append(count)
+      dMagnitude += count
+      
+   #Calculate ppmi for all entries in cooccur
+   ppmi = np.zeros((len(cooccur),len(cooccur)))
+   for i in range(len(cooccur)):
+      for j in range(len(cooccur)):
+         if i < j: #will be a symmetric matrix 
+            break
+         if cooccur[i][j] > 0:
+            pmi = math.log((cooccur[i][j]*dMagnitude)/(singleCounts[i]*singleCounts[j]),2)
+            if pmi > 0:
+               ppmi[i][j] = ppmi[j][i] = pmi
    
     print('Converting to sparse matrix...')
     ppmi = sparse.csr_matrix(ppmi)
@@ -77,12 +77,12 @@ def train_ppmi(U,S,wordList,embeddingSize,name):
 
 print('Loading data...')
 with open(EUROPARL_FILE,'r') as europarl:
-	sentences = europarl.readlines()
-	sentences = [i[:-1].split(' ') for i in sentences]
+   sentences = europarl.readlines()
+   sentences = [i[:-1].split(' ') for i in sentences]
 
 print('getWordList...')
 with open(WORDLIST_FILE,'rb') as pickleFile:
-	wordList = pickle.load(pickleFile)
+   wordList = pickle.load(pickleFile)
 
 print('buildCooccurrenceMatrix...')
 cooccur = buildCooccurrenceMatrix(sentences,wordList)
